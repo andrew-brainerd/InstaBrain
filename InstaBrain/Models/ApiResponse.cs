2cb08928fe775e2971db4f0d1c36ef2d3dc5b431
Andrew Brainerd <drwb333@gmail.com>
2018-03-18 01:23:24 -0400

    Add some other methods

---
+++ InstaBrain/Models/ApiResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaBrain.Models
+{
+    public class ApiResponse
+    {
+        public String Path { get; set; }
+        public String ResponseString { get; set; }
+        private static string baseUrl = "https://api.instagram.com";
+
+        public override string ToString()
+        {
+            return "Path: " + baseUrl + Path + "\n\nResponse: " + ResponseString;
+        }
+    }
+}
--- InstaBrain/Models/UsersClient.cs
+++ InstaBrain/Models/UsersClient.cs
@@ -36,39 +36,53 @@ namespace InstaBrain.Models
             }
         }
 
-        private static string AddAuthToRoute(string route)
-        {
-            return route + "?access_token=" + accessToken;
-        }
-
-        private static async Task<string> AuthorizedGetString(string route)
+        private static async Task<ApiResponse> AuthorizedGetString(string route, string args = "")
         {
             VerifyClient();
 
-            route = AddAuthToRoute("/v1" + route);
+            route = "/v1" + route + "?access_token=" + accessToken + (args != "" ? "&" + args : args);
+
             var response = await _client.GetAsync(route);
             var requesUuri = response.RequestMessage.RequestUri.AbsoluteUri;
-            response.EnsureSuccessStatusCode();
-
             var responseString = await response.Content.ReadAsStringAsync();
 
-            return responseString;
+            return new ApiResponse() { Path = route, ResponseString = responseString };
         }
         #endregion
 
-        public async Task<string> GetSelf()
+        public async Task<ApiResponse> GetSelf()
         {
             return await AuthorizedGetString("/users/self/");
         }
 
-        public async Task<string> GetUserById(string userId)
+        public async Task<ApiResponse> GetUserById(string userId)
         {
-            return await AuthorizedGetString("/users/" + userId);
+            return await AuthorizedGetString($"/users/{userId}");
         }
 
-        public async Task<string> GetSelfRecentMedia()
+        public async Task<ApiResponse> GetSelfRecentMedia()
         {
             return await AuthorizedGetString("/users/self/media/recent");
+        }
+
+        public async Task<ApiResponse> GetUserRecentMedia(string userId, int count = 0)
+        {
+            return await AuthorizedGetString($"/users/{userId}/media/recent");
+        }
+
+        public async Task<ApiResponse> GetSelfLikedMedia()
+        {
+            return await AuthorizedGetString("/users/self/media/liked");
+        }
+
+        public async Task<ApiResponse> GetSelfFollows()
+        {
+            return await AuthorizedGetString("/users/self/follows");
+        }
+
+        public async Task<ApiResponse> GetSelfFollowedBy()
+        {
+            return await AuthorizedGetString("/users/self/followed-by");
         }
     }
 }
